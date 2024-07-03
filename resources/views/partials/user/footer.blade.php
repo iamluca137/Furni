ff9c89fa386da0c55aa7bae1d89b59a9e45c6b80
Cactus137 <anhdo13072004@gmail.com>
2024-07-03 22:30:10 +0700

    update footer

--- resources/views/partials/user/footer.blade.php
+++ resources/views/partials/user/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer-section pb-3">
     <div class="container relative">
-        <div class="row">
+        {{-- <div class="row">
             <div class="col-lg-8">
                 <div class="subscription-form">
                     <h3 class="d-flex align-items-center"><span class="me-1"><img
@@ -23,100 +23,58 @@
 
                 </div>
             </div>
-        </div>
+        </div> --}}
 
-        <div class="row g-5 mb-5">
+        <div class="row g-5 mb-3 ">
             <div class="col-lg-4">
-                <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a>
+                <div class="mb-4 footer-logo-wrap">
+                    <a href="#" class="footer-logo fs-1">Furni<span>.</span></a>
                 </div>
-                <p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl
-                    dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor
-                    tristique. Pellentesque habitant</p>
 
-                <ul class="list-unstyled custom-social">
-                    <li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
-                    <li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
-                    <li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
-                    <li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
+                <ul class="list p-0 small"> 
+                    <li class="list-item" style="color: #707070 !important"><a href="#">Terms and Conditions</a></;><br>
+                    <li class="list-item" style="color: #707070 !important"><a href="#">Cookies and Privacy Policy</a></;><br>
                 </ul>
+
+                
             </div>
 
             <div class="col-lg-8">
-                <div class="row links-wrap">
+                <div class="row">
                     <div class="col-6 col-sm-6 col-md-3">
                         <ul class="list-unstyled">
-                            <li><a href="#">About us</a></li>
-                            <li><a href="#">Services</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Contact us</a></li>
+                            <span class="fw-bold" style="font-size: 18px">Shop</span>
+                            <li><a href="#">Shop</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
                         </ul>
                     </div>
-
                     <div class="col-6 col-sm-6 col-md-3">
                         <ul class="list-unstyled">
-                            <li><a href="#">Support</a></li>
-                            <li><a href="#">Knowledge base</a></li>
-                            <li><a href="#">Live chat</a></li>
+                            <span class="fw-bold" style="font-size: 18px">To The Customer</span>
+                            <li><a href="#">Help & FAQs</a></li>
+                            <li><a href="#">Deliveries</a></li>
+                            <li><a href="#">Returns & Refunds</a></li>
                         </ul>
                     </div>
-
                     <div class="col-6 col-sm-6 col-md-3">
                         <ul class="list-unstyled">
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Our team</a></li>
-                            <li><a href="#">Leadership</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
+                            <span class="fw-bold" style="font-size: 18px">Our Social Medias</span>
+                            <li><a href="#">Instagram</a></li>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">TikTok</a></li>
                         </ul>
                     </div>
-
                     <div class="col-6 col-sm-6 col-md-3">
                         <ul class="list-unstyled">
-                            <li><a href="#">Nordic Chair</a></li>
-                            <li><a href="#">Kruzo Aero</a></li>
-                            <li><a href="#">Ergonomic Chair</a></li>
+                            <span class="fw-bold" style="font-size: 18px">Contacts</span>
+                            <li>(+84) 382 606 012</li>
+                            <li>blackwhilee04@gmail.com</li>
+                            <li>Me Tri Ha, Tu Liem, Ha Noi</li>
                         </ul>
                     </div>
                 </div>
             </div>
-
         </div>
-
-        <div class="border-top copyright">
-            <div class="row pt-4">
-                <div class="col-lg-6">
-                    <p class="mb-2 text-center text-lg-start">Copyright &copy;
-                        <script>
-                            document.write(new Date().getFullYear());
-                        </script>
-                        <a href="." class="link-secondary">Furni</a>.
-                    </p>
-                </div>
-
-                <div class="col-lg-6 text-center text-lg-end">
-                    <ul class="list-unstyled d-inline-flex ms-auto">
-                        <li class="list-inline-item mx-2"><a href="#">Terms &amp; Conditions</a></li>
-                        <li class="list-inline-item mx-2"><a href="#">Privacy Policy</a></li>
-                        <li class="list-inline-item mx-2"><a href="https://github.com/Cactus137/Furni" target="_blank"
-                                class="link-secondary" rel="noopener">Source code</a></li>
-                        <li class="list-inline-item mx-2">
-                            <a href="https://github.com/sponsors/codecalm" target="_blank" class="link-secondary"
-                                rel="noopener">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="currentColor"
-                                    class="icon icon-tabler icons-tabler-filled icon-tabler-cactus"
-                                    style="color: green">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path
-                                        d="M7 22a1 1 0 0 1 -.117 -1.993l.117 -.007h2v-6a4 4 0 0 1 -3.995 -3.8l-.005 -.2v-1a1 1 0 0 1 1.993 -.117l.007 .117v1a2 2 0 0 0 1.85 1.995l.15 .005v-7a3 3 0 0 1 5.995 -.176l.005 .176v10a2 2 0 0 0 1.995 -1.85l.005 -.15v-5a1 1 0 0 1 1.993 -.117l.007 .117v5a4 4 0 0 1 -3.8 3.995l-.2 .005v3h2a1 1 0 0 1 .117 1.993l-.117 .007h-10z" />
-                                </svg>
-                                Cactus
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-        </div>
-
     </div>
 </footer>
